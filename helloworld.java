196456d5025ee27cc062522caf8f82060daea62a
swetha <swetha740@gmail.com>
2016-03-18 18:08:15 -0500

    my first commit in jenkins

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+
+		System.out.println("HELLO WORLD");
+		System.out.println("THIS IS MY FIRST BUILD");
+
+	}
+
+}
